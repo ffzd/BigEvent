25aefbba6a9b6451080d9491ac6418d1d85abd07
ffzd <2846778518@qq.com>
2020-08-01 14:44:46 +0800

    首页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,22 @@
 // 设置路径
 var baseURL = "http://ajax.frontend.itheima.net"
-// 拦截/过滤 每一次ajax请求，配置每次请求需要的参数
+// 1.拦截/过滤 每一次ajax请求，配置每次请求需要的参数
 $.ajaxPrefilter(function (options) {
     options.url = baseURL + options.url
     console.log(options.url)
+    // 2.判断,请求路径是否包含/my/
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+    options.complete = function (res) {
+        var data = res.responseJSON
+        console.log(res)
+        // message里面的信息和res.responseJSON.message中必需一模一样
+        if (data.status == 1 && data.message == '身份认证失败！') {
+            localStorage.removeItem("token")
+            location.href = "/login.html"
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+$(function () {
+    // 1.获取用户信息
+    getUserInfo()
+    var layer = layui.layer
+    // 点击按钮，实现退出功能
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确认要退出吗？', { icon: 3, title: '提示' }, function (index) {
+            //删除本地token
+            localStorage.removeItem("token")
+            // 页面跳转
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+// 获取用户信息封装函数
+function getUserInfo() {
+    $.ajax({
+        type: 'get',
+        url: '/my/userinfo',
+        // jQuery ajax中专门用于设置请求头信息的属性
+        // headers: {
+        //     Authorization: localStorage.getItem("token") || ""
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+// 渲染文本头像和图片头像
+function renderAvatar(user) {
+    // 1.获取用户名称
+    var name = user.nickname || user.username
+    // 2.设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 3.按需渲染用户头像
+    if (user.user_pic != null) {
+        $('.layui-nav-img').show().attr("src", user.user_pic)
+        $('.text-avator').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        $('.text-avator').show().html(name[0].toUpperCase())
+
+    }
+}
